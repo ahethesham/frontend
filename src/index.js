17b6ea7da84fa60e8d4fb038b11ba334f9e58da1
ahethesham <ahethesham8@gmail.com>
2023-07-13 12:20:17 +0530

    Deployed website

--- src/index.js
+++ src/index.js
@@ -1,268 +1,92 @@
-import React, { useState } from 'react'
-import  ReactDOM  from 'react-dom'
+import ReactDOM from 'react-dom'
+import React from 'react'
 import './index.css'
-import image from './robot.png'
-import leftarrow from './leftarrow.png'
-import rightarrow from './right-arrow.png'
-import downarrow from './down-arrow.png'
-import uparrow from './up-arrow.png'
-import resetbutton from './resetbutton.png'
-let root=ReactDOM.createRoot(document.getElementById('root'));
-let mat=[],logic=[];
-let current,reset=0;
-let instruction={
-  up:"Robot moves up",
-  down:"Robot moves down",
-  left:"Robot moves left",
-  right:"Robot moves right",
-  done:"Robot reached the end"
-}
-for(let i=0;i<5;i++){
-  let t=[];
-  for(let j=0;j<5;j++)
-  t.push(0);
-  mat.push(t);
-}
-let t=document.createElement("img")
-t.src=image
-t.className="image"
-for(let i=0;i<15;i++)logic.push(-1);
-let r;
-class Page extends React.Component{
-  constructor(){
-    super();
-    this.state={flag:reset};
-  }
-  componentDidMount(){
-    r=document.getElementById('root');
-    r.childNodes[1].childNodes[0].childNodes[0].appendChild(t)
-    r.childNodes[1].childNodes[4].childNodes[4].style.backgroundColor="red"
-    reset=0;
-  }
-  componentDidUpdate(){
-    if(this.state.flag!=reset ){
-      this.setState({flag:reset})
-      r=document.getElementById('root');
-      r.childNodes[1].childNodes[0].childNodes[0].appendChild(t)
-      r.childNodes[1].childNodes[4].childNodes[4].style.backgroundColor="red"
-    }
-  }
-  render(){
-    return(
-      <><Learn/><Grid/><Instruction val={reset}/><Logicpanel/><Controls/><Play text="play"/><Reset/></>
-    )
-  }
-}
-function Grid(){
-  return(
-    <div className='Grid'>
-    {mat.map((arr,index)=>{return <Row arr={arr} />})}
-    </div>
-  )
-}
-function Row(props){
-return(
-  <div className='Row'>
-    {props.arr.map(()=>{return <Element val={reset} />})}
-  </div>
-)
-}
-class Element extends React.Component{
-  constructor(props)
-  {
-    super(props)
-    this.state={flag:0}
-    this.componentDidUpdate=this.componentDidUpdate.bind(this);
-  }
-  componentDidUpdate(prevprops){
-  if(prevprops.val !=this.props.val)
-  {this.setState({flag:1})}
- }
-  render(){
-    return(
-      <div className='Element'> 
-      </div>
-    )
-  }
-}
-class Instruction extends React.Component{
- constructor(props){
-  super(props);
-  this.state={flag:this.props.val}
- }
- componentDidUpdate(prevprops){
-  if(prevprops.val!=this.props.val){
-    this.setState({flag:this.props.val})
-  }
- }
-  render(){
-  return (
-    <div className='instruction'>
-      <div style={{backgroundColor:'blue',height:'80px',textAlign:'center',fontSize:'30px'}}>
-      <li>Instruction Implemented</li>
-      </div>
-    </div>
-  )
-  }
-}
-function Learn(){
-return(
-  <div className='learn'>
-  Learn
-  </div>
-)
-}
-class Panelbutton extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={index:this.props.index,val:''}
-  }
-  componentDidUpdate(){
-    if(this.state.flag!=reset){
-      this.setState({flag:reset,val:'',index:this.props.index})
-    }
-  }
-  render(){
-  return(
-    <div draggable onDragOver={(e)=>{e.preventDefault();this.setState({val:current});logic[this.state.index]=current}} className='panelbutton'>
-      {this.state.val=='U' && <img src={uparrow} className='arrow1'></img>}
-      {this.state.val=='D' && <img src={downarrow} className='arrow1'></img>}
-      {this.state.val=='L' && <img src={leftarrow} className='arrow1'></img>}
-      {this.state.val=='R' && <img src={rightarrow} className='arrow1'></img>}
-      </div>
-  )
-  }
-}
-class Logicpanel extends React.Component{
+let addres;
+
+class News extends React.Component{
   constructor(){
     super()
-    this.state={flag:reset}
+    this.state={page:1}
   }
-  componentDidUpdate(){
-    if(this.state.flag!=reset)
-    this.setState({flag:reset})
-  }
-  render(){
-    const items=[];
-    for(let i=0;i<15;i++)
-    items.push(<Panelbutton index={i}/>)
-  return(
-    <div className='logicpanel' >
-      <li>Logicpanel</li>
-      {items.map((ele)=>{return ele})}
-      
-    </div>
-  )}
-
-}
-function Controls(){
-  return(
-    <div className='controls'>
-      <Controlbutton val='U'/>
-      <Controlbutton val='R'/>
-      <Controlbutton val='D'/>
-     < Controlbutton val='L'/>
-    </div>
-  )
-}
-class Controlbutton extends React.Component{
-  constructor(props){
-    super(props)
-    this.state={val:props.val}
-    this.handle=this.handle.bind(this);
-  }
-  handle(){
-    current=this.state.val;
-  }
+    
   render(){
     return(
-      <div className='controlbutton' draggable onDrag={this.handle}>
-        {this.state.val=='U'&& <img src={uparrow} className='arrow'></img>}
-        {this.state.val=='D' && <img src={downarrow}className='arrow'></img>}
-        {this.state.val=='R'&& <img src={rightarrow} className='arrow'></img>}
-        {this.state.val=='L'&& <img src={leftarrow} className='arrow'></img>}
+      <div>
+      <div className='Heading'>
+      <div className='newslogo'></div>
+      <div className='newsheading'>NEWS24</div>
+      <Search/>
+      </div>
+      <hr/>
+      <Filters/>
+      <Headlines/>
+      <div className='Pagenation'>
+        {this.state.page>1 && <button style={{height:'30px',width:'30px',backgroundColor:'black',color:'white'}} onClick={()=>{this.setState((prev)=>{return {page:prev.page-1}});addres.util(0)}}> {"<"} </button>}
+        <button style={{height:'30px',width:'30px',backgroundColor:'black',color:'white'}} onClick={()=>{this.setState((prev)=>{return {page:prev.page+1}});addres.util(1)}}>{">"}</button>
+      </div>
+      </div>
+      
+    )
+  }
+}
+class Headlines extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={headlines:[],page:0,lang:'en',q:'coding'}
+    this.util=this.util.bind(this)
+    this.handleq=this.handleq.bind(this)
+    addres=this
+  }
+  componentDidMount(){
+this.util(1) 
+ }
+util(flag){
+  if(flag==1)
+  this.setState((prev)=>{return {headlines:[],page:prev.page+1,lang:prev.lang,q:prev.q}},()=>fetch(`https://newsapi.org/v2/everything?q=${this.state.q}&language=${this.state.lang}&page=${this.state.page}&pageSize=20&apiKey=9ed751fd08904a52a2ba4593563fe7d4`).then((res)=>res.json()).then((res)=>{if(res==undefined||res.articles==undefined)return;this.setState({headlines:res.articles,page:this.state.page,lang:this.state.lang})}));
+  
+  else
+  this.setState((prev)=>{return {headlines:[],page:prev.page-1,lang:prev.lang,q:prev.q}},()=>fetch(`https://newsapi.org/v2/everything?q=${this.state.q}&language=${this.state.lang}&page=${this.state.page}&pageSize=20&apiKey=9ed751fd08904a52a2ba4593563fe7d4`).then((res)=>res.json()).then((res)=>{if(res==undefined||res.articles==undefined)return;this.setState({headlines:res.articles,page:this.state.page,lang:this.state.lang})}));
 
+}
+handleq(l){
+  this.setState({headlines:[],page:1,q:l,lang:'en'},()=>fetch(`https://newsapi.org/v2/everything?q=${this.state.q}&language=${this.state.lang}&page=${this.state.page}&pageSize=20&apiKey=9ed751fd08904a52a2ba4593563fe7d4`).then((res)=>res.json()).then((res)=>{if(res==undefined||res.articles==undefined)return;this.setState({headlines:res.articles,page:this.state.page,lang:this.state.lang})}));
+}
+
+  render(){
+    return(
+      <div>
+        {this.state.headlines!=undefined &&
+        this.state.headlines.map((news)=>{if(news.source.name!='Google News')return <><div className='headlines'><img className='newsimg' src={news.urlToImage}></img><a href={news.url} className='newstext'>{news.title}<br></br><br></br><br></br>source::{news.source.name}</a></div><br></br></>})}
       </div>
     )
   }
 }
-let row=0,col=0,i=0;
-let text=document.createElement("li");
-let util;
-function Play(props){
-function handle(){
-  if(row==4 && col==4){
-    let text=document.createElement("li");
-    text.appendChild(document.createTextNode(instruction.done));
-    r.childNodes[2].appendChild(text);
-    return;
+class Filters extends React.Component{
+  constructor(){
+    super()
+    this.state={q:['Fashion','Technology','Sports','Hollywood','Bollywood','Tesla','Apple','Programming']}
   }
-  if(i==logic.length || logic[i]==-1)return;
- 
-  setTimeout(()=>{
-    if(i==logic.length || logic[i]==-1)return;
-    if(logic[i]=='U'){
-      if(row==0)return;
-      r.childNodes[1].childNodes[row].childNodes[col].removeChild( r.childNodes[1].childNodes[row].childNodes[col].firstChild)
-      r.childNodes[1].childNodes[row-1].childNodes[col].appendChild(t);
-      let text=document.createElement("li");
-      text.appendChild(document.createTextNode(instruction.up));
-      r.childNodes[2].appendChild(text)
-      row--;
+  render(){
+    return (
+      <div className='filter'>
+        {this.state.q.map((t)=><><div className='filter_ele' onClick={()=>addres.handleq(t)}>{t}</div></>)}
+      </div>
+    )
+  }
+}
+class Search extends React.Component{
+  constructor(){
+    super();
+    this.state={t:''}
+  }
+  render(){
+    return (
+      <div className='search'>
+        <input type='text' className='searchinput' onKeyUp={(e)=>this.setState({t:e.target.value})}/>
+        <div className='searchicon' onClick={()=>addres.handleq(this.state.t)}>.</div>
+      </div>
+    )
+  }
+}
 
-    }
-    else if(logic[i]=='D'){
-      if(row==4)return;
-      r.childNodes[1].childNodes[row].childNodes[col].removeChild( r.childNodes[1].childNodes[row].childNodes[col].firstChild)
-      r.childNodes[1].childNodes[row+1].childNodes[col].appendChild(t);
-      let text=document.createElement("li");
-      text.appendChild(document.createTextNode(instruction.down));
-      r.childNodes[2].appendChild(text)
-      row++;
-    }
-    else if(logic[i]=='L'){
-      if(col==0)return;
-      r.childNodes[1].childNodes[row].childNodes[col].removeChild( r.childNodes[1].childNodes[row].childNodes[col].firstChild)
-      r.childNodes[1].childNodes[row].childNodes[col-1].appendChild(t);
-      let text=document.createElement("li");
-      text.appendChild(document.createTextNode(instruction.left));
-      r.childNodes[2].appendChild(text)
-      col--;
-    }
-    else{
-      if(col==4)return;
-      r.childNodes[1].childNodes[row].childNodes[col].removeChild( r.childNodes[1].childNodes[row].childNodes[col].firstChild)
-      r.childNodes[1].childNodes[row].childNodes[col+1].appendChild(t);
-      let text=document.createElement("li");
-      text.appendChild(document.createTextNode(instruction.right));
-      r.childNodes[2].appendChild(text)
-      col++;
-    }
-    i++;
-    handle();
-  },500);
-}
-  return(
-    <button className='play' onClick={handle}>
-      {props.text}
-    </button>
-  )
-}
-function Reset(){
-  function handle(){
-    row=0;col=0;i=0;
-    remove(r.childNodes[2])
-    reset++;
-    for(let i=0;i<15;i++)logic[i]=-1;
-    root.render(<Page/>)
-  }
-  return (
-     <button onClick={handle} className='play'>reset</button>
-  )
-}
-function remove(parent){
-  if(parent.childNodes.length==1)return;
-  parent.removeChild(parent.lastChild)
-  remove(parent);
-}
-root.render(<Page/>);
+ReactDOM.render(<News/>,document.getElementById('root'))
